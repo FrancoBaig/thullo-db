609fa46942239a431fb7ddc0574144bdb1ea9880
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-07-14 12:23:54 -0300

    Added: request logger middleware

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const app = express();
 const cors = require("cors");
 const pool = require("./mysql_connector");
 const cloudinary = require("./cloudinary/cloudinary");
+const middleware = require("./utils/middleware");
 const registerRouter = require("./router/register");
 const photoRouter = require("./router/photo");
+
 app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.listen(3001, () => {
-	console.log("Listening port 3001");
+	console.log("Listening port -> 3001");
 });
-
-app.use(express.json());
 
 app.use("/api/register", registerRouter);
 app.use("/api/photo", photoRouter);
---
+++ utils/middleware.js
@@ -0,0 +1,31 @@
+const loggerMiddle = require("./logger");
+
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:", request.path);
+	console.log("Body", request.body);
+	console.log("---");
+	next();
+};
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+	loggerMiddle.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(404).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
+	}
+
+	next(error);
+};
+
+module.exports = {
+	requestLogger,
+	unknownEndpoint,
+	errorHandler,
+};
